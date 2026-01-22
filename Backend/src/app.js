0d719bd16531832dc0dfb1d629e187300c18c833
Ritesh Chaurasia <chaurasiaritesh987@gmail.com>
2026-01-22 20:45:51 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://assesment-frontend-agfb.onrender.com',
     credentials: true
 }));
 
